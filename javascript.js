683ce4a34e18cb239fe877d972f221322c889743
Jeff Lynch <jlynch3@uw.edu>
2021-07-16 10:18:43 -0700

    first commit

--- javascript.js
+++ javascript.js
@@ -2,14 +2,14 @@ $(document).ready(function () {
   //Submit button for city search
   $(".submitBtn").on("click", function (event) {
     event.preventDefault();
-    var text = $(".weatherInput").val();
+    var text = $(".newWeather")
     console.log(text);
-    weatherSearch(text);
+    weatherInput(text);
   });
 });
 
 // Store API Key Weather
-function weatherSearch(city) {
+function weatherInput(city) {
     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=eaa146ecc747767afdb5b95a5561b7e4")
         .then(response => response.json())
         .then(data => {
